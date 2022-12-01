3116686c27ec589f4635e1bc1b82c78b6c06b50a
dyalnpahm <117327438+dyalnpahm@users.noreply.github.com>
2022-12-01 19:35:55 +1100

    Javascript

--- script.js
+++ script.js
@@ -10,5 +10,18 @@ function writePassword() {
 
 }
 
+ function generatePassword() {
+    var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var passwordLength = 12;
+    var password = "";
+ for (var i = 0; i <= passwordLength; i++) {
+   var randomNumber = Math.floor(Math.random() * chars.length);
+   password += chars.substring(randomNumber, randomNumber +1);
+  }
+        document.querySelector("password").value = password;
+ }
+
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
